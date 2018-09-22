10e40469fa66e132f512ace93e8f8b15e2dc3f56
Duvan sgf <damejia.98@hotmail.com>
2018-09-22 16:26:30 -0500

    base terminada

--- changehouse/urls.py
+++ changehouse/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
+#from changehouse.views import *
 
+app_name = 'changehouse'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.inicio, name='inicio'),
 ]
