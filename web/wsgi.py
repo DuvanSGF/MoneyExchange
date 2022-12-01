8819c5f86377c74c1b140152527d5fd0b657e1a5
Duván Mejia <software.bugs.io@gmail.com>
2022-12-01 13:34:48 -0500

    :goat: Integracion Con New Relic

--- web/wsgi.py
+++ web/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto.settings')
 
 application = get_wsgi_application()
+
+import newrelic.agent
+newrelic.agent.initialize(os.path.join(os.path.dirname(__file__), "newrelic.ini"))
+application = newrelic.agent.WSGIApplicationWrapper(application)
